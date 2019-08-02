2484fbff1925925cd2a55b5a09a72c3585bc191e
Lewis Nozaki <lhnozaki@gmail.com>
2019-08-01 20:36:26 -1000

    shopping list test 1

---
+++ public/test/shopping-list.test.js
@@ -0,0 +1,8 @@
+var expect = chai.expect;
+var should = chai.should();
+
+describe("ShoppingList", function() {
+  it("Should be a class", function() {
+    expect(ShoppingList).to.be.a("function");
+  });
+});
