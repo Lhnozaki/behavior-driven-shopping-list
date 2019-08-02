eb8c1536188dfd07733eb5c3e9ea01ef0ac53352
Lewis Nozaki <lhnozaki@gmail.com>
2019-08-01 18:25:30 -1000

    first test complete

--- public/test/shopping-list-item.test.js
+++ public/test/shopping-list-item.test.js
@@ -1,8 +1,13 @@
 var expect = chai.expect;
 var should = chai.should();
 
-describe("shoppingListItem", function() {
-  it("should be a class", function() {
-    expect(shoppingListItem).to.be.a("class");
+describe("ShoppingListItem", function() {
+  it("Should be a class", function() {
+    expect(ShoppingListItem).to.be.a("function");
+  });
+
+  it("Should have a property 'name'", function() {
+    let milk = new ShoppingListItem("milk");
+    milk.name.should.equal("milk");
   });
 });
