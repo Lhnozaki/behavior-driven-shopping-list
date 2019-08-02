eb212adaf06749c6dab1246b04bc527481ceeb1e
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-08-01 18:04:17 -1000

    first test

---
+++ public/test/shopping-list-item.test.js
@@ -0,0 +1,8 @@
+var expect = chai.expect;
+var should = chai.should();
+
+describe("shoppingListItem", function() {
+  it("should be a class", function() {
+    expect(shoppingListItem).to.be.a("class");
+  });
+});
