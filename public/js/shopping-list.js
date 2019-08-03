442353c6111fce539a1c8ec8316cbf9f04a954a8
Lewis Nozaki <lhnozaki@gmail.com>
2019-08-03 09:46:28 -1000

    pushit

--- public/js/shopping-list.js
+++ public/js/shopping-list.js
@@ -5,9 +5,25 @@ class ShoppingList {
 
   addItem(item) {
     if (!item instanceof ShoppingListItem) {
-      return error;
+      return "Please place a shopping list item";
     } else {
       this.items.push(item);
+    }
+  }
+
+  removeItem(item) {
+    let isItem = item instanceof ShoppingListItem;
+    let index = this.items.indexOf(item);
+
+    if (!isItem) {
+      return false;
+    }
+
+    if (index === -1) {
+      return false;
+    } else if (isItem && index >= 0) {
+      this.items.splice(index, 1);
+      return true;
     }
   }
 }
--- public/test/shopping-list.test.js
+++ public/test/shopping-list.test.js
@@ -17,16 +17,39 @@ describe("ShoppingList", function() {
       expect(cart.addItem).to.be.a("function");
     });
 
-    it("should place an item into the cart", function() {
+    it("Should place an item into the cart", function() {
       let cart = new ShoppingList();
       let poke = new ShoppingListItem("poke", "Spicy ahi");
       cart.addItem(poke);
       expect(cart.items).to.eql([poke]);
     });
 
-    it("should throw an error if it's not a ShoppingListItem object", function() {
+    it("Should throw an error if it's not a ShoppingListItem object", function() {
       let cart = new ShoppingList();
-      expect(cart.addItem.bind(ShoppingListItem, "cookies")).to.throw(Error);
+      cart.addItem("chicken");
+      expect(cart.addItem.bind(ShoppingListItem, "chicken")).to.throw();
+    });
+  });
+
+  describe(".removeItem", function() {
+    it("Should have a method named removeItem that accepts a single ShoppingListItem argument", function() {
+      let cart = new ShoppingList();
+      expect(cart.removeItem).to.be.a("function");
+    });
+
+    it("Should remove the item that is passed in from the shopping list", function() {
+      let cart = new ShoppingList();
+      let poke = new ShoppingListItem("poke", "Spicy Ahi");
+      let steak = new ShoppingListItem("steak", "Rib Eye");
+      let rice = new ShoppingListItem("rice", "white rice");
+      cart.addItem(poke);
+      cart.addItem(steak);
+      cart.addItem(rice);
+      cart.removeItem(steak);
+
+      expect(cart.items).to.contain(poke);
+      expect(cart.items).to.contain(rice);
+      expect(cart.items).to.not.contain(steak);
     });
   });
 });
